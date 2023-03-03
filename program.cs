35a15e23a40ae339d2293bc556db9d07ceac01ba
cpt. Pawel <87539249+ToroPawel@users.noreply.github.com>
2023-03-03 17:17:42 +0100

    420 proc*

---
+++ program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Proc420420
+{
+    class program
+    {
+        static void Main()
+        {
+            Storage.StorageTools.InitializeAllRegistry();
+            Console.Clear();
+            Console.Write("Write ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("HELP");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(" for more instructions");
+
+            while(true)
+            {
+                CommandRecevier.RecognizeCommand(CommandRecevier.ReadCommand());
+            }
+        }
+
+
+    }
+}
